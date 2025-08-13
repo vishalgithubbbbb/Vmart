1b68b176d95be1a72533c85e973098f073196431
btwitsvishuuu <teamvishal733@gmail.com>
2025-08-13 21:13:50 +0530

    small update unknown

--- Frozen/src/Components/seller/SellerLogin.jsx
+++ Frozen/src/Components/seller/SellerLogin.jsx
@@ -44,7 +44,7 @@ const SellerLogin = () => {
             </div>
             <div className="w-full ">
                 <p>Password</p>
-                <input onChange={(e) => setPassword(e.target.value)} value={password} placeholder="type here" className="border border-gray-200 rounded w-full p-2 mt-1 outline-indigo-500" type="password" required />
+                <input onChange={(e) => setPassword(e.target.value)} value={password} placeholder="type here" className="border border-gray-200 rounded w-full p-2 mt-1 outline-indigo-500" type="password" autoComplete="current-password" required />
             </div>
             <button className="bg-indigo-500 hover:bg-indigo-600 transition-all text-white w-full py-2 rounded-md cursor-pointer">
               Login
