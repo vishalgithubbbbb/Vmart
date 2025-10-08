47bb03ed129217f4639246e295b35d99a4b40c3e
btwitsvishuuu <teamvishal733@gmail.com>
2025-10-08 22:23:30 +0530

    multer updates

--- Backend/config/multer.js
+++ Backend/config/multer.js
@@ -1,22 +1,10 @@
 import multer from "multer";
 
-import fs from "fs";
-
-// ✅ Use serverless-safe temp directory
-const uploadDir = "/tmp/uploads";
-
-// Create the directory if it doesn't exist
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
-}
-
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDir);
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  }
-});
+  destination:"tmp/uploads",filename:(req,file,cb)=>{
+   return cb(null, `${Date.now()}${file.originalname}`)
+    }
+})
 
-export const upload = multer({ storage });
+export const upload = multer({storage:storage});
+
