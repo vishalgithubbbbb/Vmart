c1d3a7ff74a8535b88261f84e67a638d9809875b
Vishal <vishalchaurasiya1408@gmail.com>
2026-07-03 00:38:30 +0530

    Adding for AWS Services

--- Backend/config/connectdb.js
+++ Backend/config/connectdb.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
 
-export const connectDB = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Mongodb connected")
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ Connected to DocumentDB");
+  } catch (err) {
+    console.error("❌ Error connecting to DocumentDB:", err);
+    process.exit(1);
+  }
+};
 
-    } 
-    catch(error){
-        console.log("Error connecting to MongoDB:",error);
-        process.exit(1); //Exit the process with failure
-
-    }
-}
+export default connectDB;
